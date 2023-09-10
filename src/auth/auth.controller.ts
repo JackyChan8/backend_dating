740f668378c57e4119ea41acbd8265244f0751f4
rob-bender <rob_bender@inbox.lv>
2023-09-10 05:59:57 -0700

    little modify mail service (add dto)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('sign-up')
+  async signUp() {
+    await this.authService.signUp();
+  }
+
+  @Post('sign-in')
+  async signIn() {
+    await this.authService.signIn();
+  }
+
+  @Post('logout')
+  async logout() {
+    await this.authService.logout();
+  }
+
+  @Post('refresh')
+  async refresh() {
+    await this.authService.refresh();
+  }
+}
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,20 +1,22 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 
+import { SendActivationMailBody } from './dto/send-activation-mail.dto';
+
 @Injectable()
 export class MailService {
   constructor(private readonly mailerService: MailerService) {}
 
-  async sendActivationMail(to: string, link: string): Promise<boolean> {
+  async sendActivationMail(dto: SendActivationMailBody): Promise<boolean> {
     const response = await this.mailerService.sendMail({
       from: 'rob-nerd@yandex.ru',
-      to: to,
+      to: dto.to,
       subject: 'Подтверждение аккаунта',
       text: '',
       html: `
       <div>
 					<h1>Для активации перейдите по ссылке</h1>
-					<a href="${link}">Ссылка</a>
+					<a href="${dto.link}">Ссылка</a>
 				</div>
       `,
     });
